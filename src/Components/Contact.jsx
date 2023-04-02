1cf1b37e34f522c2c80918324d73fcf2b132f6b7
Ayan Kar <ayankar84@gmail.com>
2023-04-02 13:38:04 +0530

    download working

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,14 @@
+import 'boxicons'
+
 function Contact() {
     return (
         <section class="contact" id="contact">
             <h2 class="heading">Contact <span>Me!</span></h2>
+            <div className="social-media" style={{ "display": "flex", "justifyContent": "center", "alignItems": "center", "marginTop": "-4rem" }}>
+                <a href="https://github.com/Ayankar84" target="_blank"><box-icon type='logo' color="white" name='github' /></a>
+                <a href="https://www.linkedin.com/in/ayan-kar-43625421b/" target="_blank"><box-icon color="white" type='logo' name='linkedin' /></a>
+                <a href="tel:+918436463854"><box-icon color="white" name='phone-call' /></a>
+            </div>
             <form action="#">
                 <div class="input-box">
                     <input type="text" placeholder="Full Name" />
